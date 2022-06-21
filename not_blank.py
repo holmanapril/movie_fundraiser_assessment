545f11ca15add3b45793770f1a091e443b0a84a7
aprilholman <holman.april@awatapu.school.nz>
2022-06-21 13:42:51 +1200

    Turns previous code into a function and prints the name inside the function

--- not_blank.py
+++ not_blank.py
@@ -1,12 +1,19 @@
-valid = False
-# Keeps running while loop until they enter a name that is not a digit
-while not valid:
-    # Asks what users name is
-    name = str(input("What is your name?")).title()
-    if name.isdigit():
-        # Prints error message
-        print("Please enter a valid name")
-    else:
-        valid = True
-        # Prints name entered
-        print(name)
+# Functions
+def name():
+    valid = False
+    # Keeps running loop until user inputs a valid name
+    while not valid:
+        # Asks them their name
+        user_name = str(input("What is your name?")).title()
+        # Checks if input is a number
+        if user_name.isdigit():
+            # If input is a number it prints error message
+            print("Please enter a valid name")
+        else:
+            # Prints users name
+            print(user_name)
+            return user_name
+
+
+# Main Routine
+name()
