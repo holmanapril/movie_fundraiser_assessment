49e7d1b6571b7801225ab6a92ee7a360f20e1529
aprilholman <holman.april@awatapu.school.nz>
2022-06-28 14:57:47 +1200

    Forces the user to input a valid not letters age using try/ except

--- user_age.py
+++ user_age.py
@@ -1,11 +1,20 @@
-# Asks for users age
-age = int(input("How old are you?"))
-if age <= 11:
-    # User too young
-    print("You are too young to be doing this")
-elif age >= 131:
-    # User too old
-    print("Please enter a valid age between(12 and 130)")
-else:
-    # valid age
-    print(age)
+valid = False
+# Continues to ask for input until valid input it entered
+while not valid:
+    try:
+        # Asks for age
+        age = int(input("How old are you?"))
+        if age <= 11:
+            # If too young, code will finish
+            print("You are too young to be doing this")
+            valid = True
+        elif age >= 131:
+            # If too old, user will be re-asked
+            print("Please enter a valid age between(12 and 130)")
+        else:
+            # If valid age, code finishes
+            print(age)
+            valid = True
+    except ValueError:
+        # Prints error message if string is input
+        print("Please enter a valid age(between 12 and 130)")
