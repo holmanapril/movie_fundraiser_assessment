9033cad03b3ca1c8683f789270af7c3e16fc5138
aprilholman <holman.april@awatapu.school.nz>
2022-06-28 14:40:11 +1200

    This code counts the amount of spaces in the name entered, to make sure user inputs their full name. But the code doesn't wokr because when being checked if it is has all letters the space is not a letter so the error message is printed

--- not_blank.py
+++ not_blank.py
@@ -3,16 +3,26 @@ def not_blank(question, error):
     valid = False
     # Keeps running loop until user inputs a valid name
     while not valid:
+        count = 0
         # Asks question
-        user_name = str(input(question)).title()
-        # Makes sure input isn't a number or blank
-        if user_name.isnumeric() or user_name == "":
-            # If digit or blank, error message prints
+        user_name = str(input(question)).title().strip()
+        # Counts how many spaces there are in user_name
+        for i in range(len(user_name)):
+            if user_name[i].isspace():
+                count += 1
+        print(count)
+        # Doesn't let user enter a name that has
+        if count <= 0:
             print(error)
         else:
-            print(user_name)
-            return user_name
+            # Makes sure input isn't a number or blank
+            if user_name.isalpha() is False or user_name.isnumeric() or user_name == "":
+                # If digit or blank, error message prints
+                print(error)
+            else:
+                print(user_name)
+                return user_name
 
 
 # Main Routine
-not_blank("What is your name?", "Please enter a valid name")
+not_blank("What is your name?", "Please enter a valid full name(first and last name)")
