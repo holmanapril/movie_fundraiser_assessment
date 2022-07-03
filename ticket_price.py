041f4e4ac1e728f78cb9551589b4ae80a04bcf07
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 11:48:26 +1200

    Lets user input data for age, does not accept invalid data

--- ticket_price.py
+++ ticket_price.py
@@ -1,11 +1,11 @@
-age = 32
+age = int(input("How old are you?"))
 cost = 0
 if age < 16:
     cost += 7.5
-elif age > 15 or age < 65:
+elif 16 <= age <= 64:
     cost += 10.5
 else:
     cost += 6.5
 
-
+print(age)
 print(cost)
