4dd21208df88365c9f4a45fbac8fb587db8b753a
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 11:40:02 +1200

    Adds parameters to make error messages more efficient

--- user_age.py
+++ user_age.py
@@ -1,26 +1,27 @@
 # Functions
-def user_age():
+def user_age(question, error, error_2):
     valid = False
     # Continues to ask for input until valid input it entered
     while not valid:
         try:
             # Asks for age
-            age = int(input("How old are you?"))
+            age = int(input(question))
             if age <= 11:
                 # If too young, code will finish
-                print("You are too young to be doing this")
-                valid = True
+                print(error_2)
+                return age
             elif age >= 131:
                 # If too old, user will be re-asked
-                print("Please enter a valid age between(12 and 130)")
+                print(error)
             else:
                 # If valid age, code finishes
                 print(age)
-                valid = True
+                return age
         except ValueError:
             # Prints error message if string is input
-            print("Please enter a valid age(between 12 and 130)")
+            print(error)
 
 
 # Main routine
-user_age()
+user_age("How old are you?", "Please enter a valid age between(12 and 130)",
+         "You are too young to be doing this")
