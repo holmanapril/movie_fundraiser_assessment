7bc5127887ce0fa12d2e95c9c280cd05989f8c7b
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 11:51:42 +1200

    using try and accept it forces valid input

--- ticket_price.py
+++ ticket_price.py
@@ -1,11 +1,20 @@
-age = int(input("How old are you?"))
-cost = 0
-if age < 16:
-    cost += 7.5
-elif 16 <= age <= 64:
-    cost += 10.5
-else:
-    cost += 6.5
+valid = False
+while not valid:
+    try:
+        age = int(input("How old are you?"))
+        cost = 0
+        if age < 16:
+            cost += 7.5
+            valid = True
+        elif 16 <= age <= 64:
+            cost += 10.5
+            valid = True
+        else:
+            cost += 6.5
+            valid = True
+    except ValueError:
+        print("Please enter a valid age")
+
 
 print(age)
 print(cost)
