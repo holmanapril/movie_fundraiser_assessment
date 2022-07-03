da8e089aa633efda57c75b22eee514bb846ea9ce
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 11:45:29 +1200

    Adds user_age

--- movie_fundraiser.py
+++ movie_fundraiser.py
@@ -26,5 +26,30 @@ def not_blank(question, error):
                 print(error)
 
 
+def user_age(question, error, error_2):
+    valid = False
+    # Continues to ask for input until valid input it entered
+    while not valid:
+        try:
+            # Asks for age
+            age = int(input(question))
+            if age <= 11:
+                # If too young, code will finish
+                print(error_2)
+                return age
+            elif age >= 131:
+                # If too old, user will be re-asked
+                print(error)
+            else:
+                # If valid age, code finishes
+                print(age)
+                return age
+        except ValueError:
+            # Prints error message if string is input
+            print(error)
+
+
 # Main Routine
 not_blank("What is your name?", "Please enter a valid full name(first and last name)")
+user_age("How old are you?", "Please enter a valid age between(12 and 130)",
+         "You are too young to be doing this")
