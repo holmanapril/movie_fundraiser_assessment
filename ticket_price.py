9e4ac50a6cbcba11002ff883f3e311f14975f700
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 11:53:11 +1200

    Turned into function

--- ticket_price.py
+++ ticket_price.py
@@ -1,20 +1,28 @@
-valid = False
-while not valid:
-    try:
-        age = int(input("How old are you?"))
-        cost = 0
-        if age < 16:
-            cost += 7.5
-            valid = True
-        elif 16 <= age <= 64:
-            cost += 10.5
-            valid = True
-        else:
-            cost += 6.5
-            valid = True
-    except ValueError:
-        print("Please enter a valid age")
+def ticket_price(question, error):
+    cost = 0
+    age = 0
+    # Asks user what their age is
+    print(question)
+    valid = False
+    while not valid:
+        try:
+            age = int(input())
+            # If age is less than 16 the code finishes
+            if age < 16:
+                cost += 7.5
+                valid = True
+            # if age between 16 and 64 cost = 10.5 and finishes
+            elif 16 <= age <= 64:
+                cost += 10.5
+                valid = True
+            else:
+                cost += 6.5
+                valid = True
+        # If invalid input is entered error message is printed
+        except ValueError:
+            print(error)
+    print(age)
+    print(cost)
 
 
-print(age)
-print(cost)
+ticket_price("How old are you?", "Please enter a valid age")
