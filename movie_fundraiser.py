f941134c4b452c38c39bdb71c71f6357cb72cc97
aprilholman <holman.april@awatapu.school.nz>
2022-07-04 12:05:41 +1200

    added ticket_price

--- movie_fundraiser.py
+++ movie_fundraiser.py
@@ -49,7 +49,35 @@ def user_age(question, error, error_2):
             print(error)
 
 
+def ticket_price(question, error):
+    cost = 0
+    age = 0
+    # Asks user what their age is
+    print(question)
+    valid = False
+    while not valid:
+        try:
+            age = int(input())
+            # If age is less than 16 the code finishes
+            if age < 16:
+                cost += 7.5
+                valid = True
+            # if age between 16 and 64 cost = 10.5 and finishes
+            elif 16 <= age <= 64:
+                cost += 10.5
+                valid = True
+            else:
+                cost += 6.5
+                valid = True
+        # If invalid input is entered error message is printed
+        except ValueError:
+            print(error)
+    print(age)
+    print(cost)
+
+
 # Main Routine
 not_blank("What is your name?", "Please enter a valid full name(first and last name)")
 user_age("How old are you?", "Please enter a valid age between(12 and 130)",
          "You are too young to be doing this")
+ticket_price("How old are you?", "Please enter a valid age")
