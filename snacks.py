f438c25c7c8f933c0b45ca01350880dc3ab3f74d
aprilholman <holman.april@awatapu.school.nz>
2022-07-05 12:38:00 +1200

    Asks if snacks wanted, how many and finishes if no snacks are wanted

--- snacks.py
+++ snacks.py
@@ -1,44 +1,42 @@
-# Functions
-def snacks(question, error, question_2, error_2, question_3):
+def snacks(question_1, question_2, question_3, error):
+    snack_price_total = 0
     # Snack choices
     snack_choices = ["Popcorn", "M&M", "Pitachips", "Orangejuice", "Water"]
-    keep_going = False
-    # Keeps asking if user wants more snacks
-    while keep_going is False:
-        # Asks if user want snacks
-        snack_yes_no = str(input(question_3)).lower().strip()
-        if snack_yes_no == "y" or snack_yes_no == "yes":
-            valid = False
-            print(question)
-            while not valid:
-                # What snacks are wanted
-                user_choice = str(input()).title().strip()
-                if user_choice not in snack_choices:
-                    print(error)
-                else:
-                    # Reprints users input to check
-                    print(user_choice)
-                    valid = False
-                    while not valid:
-                        try:
-                            # Asks how many loops until valid input entered
-                            user_snack_amount = int(input(question_2))
-                            if user_snack_amount <= 0:
-                                print(error_2)
-                            # Limit is 5
-                            elif user_snack_amount >= 6:
-                                print(error_2)
-                            else:
-                                print(user_snack_amount)
-                                valid = True
-                        except ValueError:
-                            print(error_2)
-        else:
-            # if user answers no code finishes
-            keep_going = True
+    # Snack prices
+    snack_prices = [2.50, 3.00, 4.50, 3.25, 2.00]
+    valid = False
+    while not valid:
+        options = False
+        try:
+            # Asks if more snacks wanted
+            yes_no = str(input(question_1)).strip().lower()
+            if yes_no == "y" or yes_no == "yes":
+                while not options:
+                    # User inputs corresponding number of snack
+                    user_choice = int(input(question_2))
+                    # User inputs snack amount
+                    user_snack_amount = int(input(question_3))
+                    # Errors if invalid input entered
+                    if user_choice >= 5 or user_choice < 0 or user_snack_amount > 5 or user_snack_amount < 1:
+                        print(error)
+                    else:
+                        # Calculates snack pricing
+                        snack_price = snack_prices[user_choice - 1] * user_snack_amount
+                        # Adds to total
+                        snack_price_total += snack_price
+                        # Prints snack price of snack just chosen
+                        print("${:.2f}".format(snack_price))
+                        # Prints choices to check
+                        print("And your choices were {} {}".format(user_snack_amount, snack_choices[user_choice - 1]))
+                        options = True
+            elif yes_no == "n" or yes_no == "no":
+                print("Total price of your snacks is: ${}".format(snack_price_total))
+                return snack_price_total
+        except ValueError:
+            print(error)
 
 
-# Main routine
-snacks("What snack would you like?", "Please enter a valid snack choice(Please do not put spaces between words",
-       "How many would you like?", "Please enter a valid amount(The maximum you can order of each snack is 5)",
-       "Would you like to order any/more snacks?\nThe Options are:\nPopcorn\nM&M\nPita chips\nOrange Juice\nWater?")
+snacks("Do you want to order some/more snacks?", "Pick a snack(pick the number you want)\nThe options are:\n"
+       "1. Popcorn: $2.50\n2. M&M: $3.00\n"
+       "3. Pitachips: $4.50\n4.Orange Juice: $3.25\n5. Water: $2.00", "Choose an amount(maximum is 5)",
+       "Please enter a valid snack number")
