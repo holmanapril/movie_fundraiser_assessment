2192ac26aaa8bf694749febd4cd190ac8788deb6
aprilholman <holman.april@awatapu.school.nz>
2022-07-08 09:41:37 +1200

    made it so that users can order more than one ticket at a time

--- ticket_price.py
+++ ticket_price.py
@@ -1,28 +1,52 @@
-def ticket_price(question, error):
+def ticket_price_amount(question, error, error_2):
     cost = 0
-    age = 0
-    # Asks user what their age is
-    print(question)
+    count = 1
+    t_amount = 0
     valid = False
+    valid_ticket_amount = False
+    tickets_available = 3
+    print("There are {} tickets available".format(tickets_available))
+    # Asks how many tickets user wants
     while not valid:
         try:
-            age = int(input())
-            # If age is less than 16 the code finishes
-            if age < 16:
-                cost += 7.5
+            while not valid_ticket_amount:
+                t_amount = int(input(question))
+                if t_amount > tickets_available:
+                    print(error_2)
+                    print("There are {} tickets available".format(tickets_available))
+                else:
+                    valid_ticket_amount = True
+                    print()
+
+            while count < t_amount + 1:
+                try:
+                    # Asks for age
+                    age = int(input("What age will ticket {} be for?".format(count)))
+                    if age <= 11:
+                        # If too young, user will be re_asked
+                        print(error)
+                    elif age >= 131:
+                        # If too old, user will be re-asked
+                        print(error)
+                    else:
+                        if age < 16:
+                            print("Ticket {} will cost $7.50\n".format(count))
+                            cost += 7.5
+                            count += 1
+                        elif 16 <= age <= 64:
+                            print("Ticket {} will cost $10.50\n".format(count))
+                            cost += 10.5
+                            count += 1
+                        else:
+                            print("Ticket {} will cost $6.50\n".format(count))
+                            cost += 6.5
+                            count += 1
+                except ValueError:
+                    print(error)
                 valid = True
-            # if age between 16 and 64 cost = 10.5 and finishes
-            elif 16 <= age <= 64:
-                cost += 10.5
-                valid = True
-            else:
-                cost += 6.5
-                valid = True
-        # If invalid input is entered error message is printed
         except ValueError:
             print(error)
-    print(age)
-    print(cost)
 
 
-ticket_price("How old are you?", "Please enter a valid age")
+ticket_price_amount("How many tickets would you like?", "Please enter a valid age",
+                    "Please enter a valid ticket amount")
