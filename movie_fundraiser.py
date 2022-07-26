fd07129f2689a824019a1411a4a52e824ae07ad0
aprilholman <holman.april@awatapu.school.nz>
2022-07-26 14:05:30 +1200

    adds list code and removes a global statement

--- movie_fundraiser.py
+++ movie_fundraiser.py
@@ -20,6 +20,7 @@ def not_blank(question, error):
             if spaces >= 1:
                 # If spaces is more than 0, it reprints name and ends the function
                 print(user_name)
+                current_user_list[0] = user_name
                 return user_name
             else:
                 # If spaces = 0, it prints error and asks again
@@ -47,6 +48,7 @@ def ticket_price_amount(question, error, error_2):
                     print("There are {} tickets available".format(tickets_available))
                 else:
                     valid_ticket_amount = True
+                    current_user_list[1] = t_amount
                     print()
 
             while count < t_amount + 1:
@@ -77,6 +79,7 @@ def ticket_price_amount(question, error, error_2):
                 valid = True
         except ValueError:
             print(error)
+    current_user_list[2] = cost
     print("Total cost of all tickets is ${:.2f}".format(cost))
 
 
@@ -111,11 +114,27 @@ def snacks(question_1, question_2, question_3, error):
                         print("${:.2f}".format(snack_price))
                         # Prints choices to check
                         print("And your choices were {} {}".format(user_snack_amount, snack_choices[user_choice - 1]))
+                        if user_choice == 1:
+                            current_user_list[3] += user_snack_amount
+                            summary_details[1] += user_snack_amount
+                        elif user_choice == 2:
+                            current_user_list[4] += user_snack_amount
+                            summary_details[2] += user_snack_amount
+                        elif user_choice == 3:
+                            current_user_list[5] += user_snack_amount
+                            summary_details[3] += user_snack_amount
+                        elif user_choice == 4:
+                            current_user_list[6] += user_snack_amount
+                            summary_details[4] += user_snack_amount
+                        else:
+                            current_user_list[7] += user_snack_amount
+                            summary_details[5] += user_snack_amount
                         options = True
             elif yes_no == "n" or yes_no == "no":
                 # Prints total price of all ordered snacks
                 print("Total price of your snacks is: ${}".format(snack_price_total))
                 cost += snack_price_total
+                current_user_list[8] = snack_price_total
                 return snack_price_total
         except ValueError:
             print(error)
@@ -123,6 +142,7 @@ def snacks(question_1, question_2, question_3, error):
 
 def payment(question, error):
     global cost
+    global payment_method
     valid = False
     # Prints question
     print(question)
@@ -133,11 +153,13 @@ def payment(question, error):
             if payment_method == 1:
                 # 1 corresponds to cash payment
                 payment_method = "Cash"
+                current_user_list[9] = payment_method
                 return payment_method
             elif payment_method == 2:
                 # 2 corresponds to credit payment
                 payment_method = "Credit"
                 cost += cost * 0.05
+                current_user_list[9] = payment_method
                 return payment_method
             else:
                 print(error)
@@ -146,17 +168,22 @@ def payment(question, error):
 
 
 def profit():
-    global cost
     global t_amount
     global profit_per_user
+    profit_per_user = 0
     # Times profit per ticket by amount of tickets
     profit_per_user += 5.5 * t_amount
+    summary_details[0] += profit_per_user
 
 
 # Main routine
 global cost
 global t_amount
 global profit_per_user
+global payment_method
+current_user_list = ["Name", "ticket amount", "total ticket price", 0, 0, 0, 0, 0,
+                     "snack price", "payment method", "total price"]
+summary_details = [0, 0, 0, 0, 0, 0]
 not_blank("What is your name?", "Please enter a valid full name(first and last name)")
 ticket_price_amount("How many tickets would you like?", "Please enter a valid age",
                     "Please enter a valid ticket amount")
@@ -170,5 +197,8 @@ payment("Will you be paying cash or credit?(enter 1 or 2)\nIf paying with credit
         "\nOption 1                "
         "Option 2\nCash                    Credit")
 profit()
+current_user_list[10] = cost
 print("Total cost = ${:.2f}".format(cost))
 print("Profit = ${:.2f}".format(profit_per_user))
+print(current_user_list)
+print(summary_details)
