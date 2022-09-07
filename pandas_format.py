6529d33c42d75dedd88f8af78afa317bb3e33d03
aprilholman <holman.april@awatapu.school.nz>
2022-09-08 09:36:25 +1200

    printed pandas tables to check they work

--- pandas_format.py
+++ pandas_format.py
@@ -214,4 +214,5 @@ current_ticket = pd.DataFrame(current_user_list, columns=["Name", "Ticket Amount
                                                           "Order total"])
 total_summary = pd.DataFrame(summary_details, columns=["Profit", "Popcorn", "M&M's", "Pita Chips", "Orange Juice",
                                                        "Water"])
-
+print(current_ticket)
+print(total_summary)
